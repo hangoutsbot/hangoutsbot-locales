cc32855627bb5512a0548768e2e447fee5d95e9e
null pointer <eol+temporary-2@invalidfile.name>
2015-07-29 19:06:02 +0800

    fixed update-messages.sh to allow relative path calling updated latest messages

--- update-messages.sh
+++ update-messages.sh
@@ -3,5 +3,5 @@ DIR="`dirname $0`/"
 xgettext --files-from="$DIR"POTFILES.in --directory="$DIR".. --output="$DIR"messages.pot
 echo "messages.pot generated in $DIR"
 rm -rf "$DIR"en/LC_MESSAGES/hangupsbot.po
-msgen messages.pot --directory="$DIR" -o en/LC_MESSAGES/hangupsbot.po
+msgen messages.pot --directory="$DIR" -o "$DIR"en/LC_MESSAGES/hangupsbot.po
 echo "and completed the en file"
